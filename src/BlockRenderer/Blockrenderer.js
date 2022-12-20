644e23bf094c34e88d0d18e47bfa25c92b723439
Lorena Echeverri <lorena.echeverry@hotmail.com>
2022-12-20 17:05:24 +0100

    blockrender

---
+++ src/BlockRenderer/Blockrenderer.js
@@ -0,0 +1,12 @@
+
+
+export default function BlockRenderer({blocks}) {
+    // if(!blocks) return <></>
+    console.log(blocks)
+    // return blocks?.map((block, index) => {
+    //     switch('block.acf_fc_layout') {
+    //         case 'test':
+    //             return <div key={index}>Test</div>
+    //     }
+    // })
+}
