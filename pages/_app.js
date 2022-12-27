db3a125357b08e0af2c0d99ef469bf49bd5e0b6b
Lorena Echeverri <lorena.echeverry@hotmail.com>
2022-12-27 19:10:04 +0100

    cart/product

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
 import { LayoutHandler } from '../src/utils/LayoutHandler'
 import Menu from '../src/components/menu'
 import '../styles/style.scss'
+import {  useState, useMemo } from 'react'
+import { CartContext } from '../src/Context/cartContext'
 
 function MyApp({ Component, pageProps }) {
   LayoutHandler.listen()
+  const [cart, setcart] = useState([])
+  const cartValue = useMemo(() => ({cart, setcart}), [cart, setcart])
+  console.log(cart)
   return(
-    <>
+    <CartContext.Provider value={cartValue}>
     <Menu/>
     <Component {...pageProps} />
-    </>
+    </CartContext.Provider>
   ) 
 }
 
--- pages/product/[product_slug].js
+++ pages/product/[product_slug].js
@@ -1,9 +1,80 @@
 import React from "react"
 import Woocommerce from "../../src/WooCommerce"
+import { useContext, useState } from "react";
+import { CartContext } from "../../src/Context/cartContext";
+import Link from "next/link";
 
 
 
-export async function getServerSideProps(context){
+
+export default function Category({product}) {
+  const {cart, setcart} = useContext(CartContext)
+  const [qty, setQty] = useState(1)
+    // console.log(product)
+   
+    const addToCart = () => {
+
+      for(let i = 0; i < cart.length; i++) {
+        let cartProduct = cart[i]
+        if(cartProduct.id === product.id) {
+          setcart( [...cart,{...cart[i], quantity: cart[i].quantity + qty}]) 
+          console.log(cart[i])
+        }
+      }
+      console.log(cart)
+      let exsistInCart = false;
+      for (const cartProduct of cart) {
+        if(cartProduct.id === product.id) {
+          exsistInCart = true;
+         
+        }
+      }
+      if(!exsistInCart) {
+        setcart([...cart, {
+          'img': product.images[0].src,
+          'price': product.price,
+          'title': product.name,
+          'id': product.id,
+          'quantity': qty
+         }])
+      } 
+      
+    }
+
+    const incrementQty = () => {
+      setQty(qty + 1)
+    }
+
+    const decrementQty = () => {
+      if(qty>1) {
+        setQty(qty - 1)
+      }
+    }
+
+    return (
+      <div>
+        {/* //map för att få ut produktkort  */}
+        {/* {produkter.map((produkt, index) => {
+          <Card 
+          key={index}
+        title={produkt.title}
+        ig={produkt.title}
+        />
+        })} */}
+        <button onClick={addToCart}>Add to cart</button>
+        <img src={product.images[0].src} />
+        <h1>{product.name}</h1>
+        <Link href={"/product/golvlampa-neteyam"}>Nateyam</Link>
+        <Link href={"/product/bordslampa-moberg"}>Moberg</Link>
+        <button onClick={decrementQty} >-</button>
+        {qty}
+        <button onClick={incrementQty}>+</button>
+      </div>
+    )
+  }
+
+
+  export async function getServerSideProps(context){
     let slug = context.params.product_slug;
     let product = await Woocommerce.getProductBySlug(slug);
 
@@ -14,19 +85,3 @@ export async function getServerSideProps(context){
         }
     }
 }
-
-
-export default function Category(props) {
-    console.log(props)
-    
-    
-
-   
-    
-  
-    return (
-      <div>
-        <div className="h1">Hej Produkt</div>
-      </div>
-    )
-  }
